90de5a5d7777c30b710ecd74d0001f4f83f90ec4
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-24 20:11:06 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    document.title = `Count (${counter})`;
+  }, [counter]);
+
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Count ({counter})</button>
     </div>
   );
 }
